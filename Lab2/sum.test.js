19ac02e6a316a4f8981c7d183d8bead1e6c6f239
darraghstudent <l00194487@atu.ie>
2024-12-01 19:33:23 +0000

    Create sum.test.js

---
+++ Lab2/sum.test.js
@@ -0,0 +1,9 @@
+const sum = (a, b) => a + b;
+
+test('adds 1 + 2 to equal 3', () => {
+  expect(sum(1, 2)).toBe(3);
+});
+
+test('adds -1 + -1 to equal -2', () => {
+  expect(sum(-1, -1)).toBe(-2);
+});
