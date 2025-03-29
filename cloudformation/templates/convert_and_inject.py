bce82762683d4277240ff49ab4a346565eb172fe
darraghstudent <l00194487@atu.ie>
2025-03-29 12:37:38 +0000

    update

--- cloudformation/templates/convert_and_inject.py
+++ cloudformation/templates/convert_and_inject.py
@@ -1,4 +1,3 @@
-import yaml
 import json
 import os
 
@@ -61,29 +60,24 @@ def convert_and_inject(task_def_file, output_file):
     for container in ecs_task_def.get("containerDefinitions", []):  # Note: PascalCase preserved
         for env_var in container.get("environment", []):  # Note: PascalCase preserved
             name = env_var.get("name")
-            if name == "db_password":
+            if name == "DB_PASSWORD":
                 env_var["value"] = os.getenv("DB_PASSWORD", "default-password")  # GitHub Secret or default
-            elif name == "vpc_id":
+            elif name == "VpcId":
                 env_var["value"] = os.getenv("VPC_ID", "")
-            elif name == "task_exec_role_arn":
+            elif name == "TaskExecutionRoleArn":
                 env_var["value"] = os.getenv("TASK_EXEC_ROLE", "")
-            elif name == "subnet_id":
-                env_var["value"] = os.getenv("SUBNET_ID", "")
-            elif name == "aws_region":
-                env_var["value"] = os.getenv("AWS_REGION", "us-west-1")  # Default region if not set
-            elif name == "db_host":
+            elif name == "PublicSubnetId":
+                env_var["value"] = os.getenv("PublicSubnetId", "")
+            elif name == "AWS_REGION":
+                env_var["value"] = os.getenv("AWS_REGION", "eu-west-1")  # Default region if not set
+            elif name == "DB_HOST":
                 env_var["value"] = os.getenv("DB_HOST", "localhost")  # Default host if not set
-            elif name == "db_port":
+            elif name == "DB_PORT":
                 env_var["value"] = os.getenv("DB_PORT", "5432")  # Default port if not set
 
     # Write the final ECS task definition to a JSON file
     with open(output_file, 'w') as json_file:
         json.dump(ecs_task_def, json_file, indent=2)
-
-if __name__ == "__main__":
-    task_def_file = os.getenv('TASK_DEF_FILE', 'cloudformation/templates/Task_def.yml')
-    output_file = 'Task_def.json'
-    convert_and_inject(task_def_file, output_file)
 
 if __name__ == "__main__":
     task_def_file = os.getenv('TASK_DEF_FILE', 'cloudformation/templates/Task_def.yml')
